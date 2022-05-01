07b0ca994d2db63a89ea69219ecef65d51a0db96
Bereczki Sándor Máté <bsandor453@gmail.com>
2022-05-01 06:04:41 +0200

    Added example dependency injection code

--- app/src/main/java/hu/bme/cryptochecker/modules/Network.kt
+++ app/src/main/java/hu/bme/cryptochecker/modules/Network.kt
@@ -1,10 +1,19 @@
 package hu.bme.cryptochecker.modules
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class Network {
+
+    @Singleton
+    @Provides
+    @Named("DummyNetworkService")
+    fun dummyNetworkService() = "This is a dummy network service"
+
 }
--- app/src/main/java/hu/bme/cryptochecker/ui/main/MainActivity.kt
+++ app/src/main/java/hu/bme/cryptochecker/ui/main/MainActivity.kt
@@ -2,9 +2,18 @@ package hu.bme.cryptochecker.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import hu.bme.cryptochecker.R
+import javax.inject.Inject
+import javax.inject.Named
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    @Named("DummyNetworkService")
+    lateinit var networkService: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
