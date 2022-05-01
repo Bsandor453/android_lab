50b34e2a1b30e30a06fcd00e5ce93a720a4ae161
Bereczki Sándor Máté <bsandor453@gmail.com>
2022-05-01 05:43:47 +0200

    Added repository module

---
+++ app/src/main/java/hu/bme/cryptochecker/modules/Repository.kt
@@ -0,0 +1,11 @@
+package hu.bme.cryptochecker.modules
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class Repository {
+}
