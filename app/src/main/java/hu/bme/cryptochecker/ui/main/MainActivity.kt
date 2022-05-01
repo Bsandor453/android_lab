e0d6ad685482500712f2b6cbe1a8868a5ef91cc2
Bereczki Sándor Máté <bsandor453@gmail.com>
2022-05-01 06:16:42 +0200

    Added example viewmodel inject

--- app/src/main/java/hu/bme/cryptochecker/ui/main/MainActivity.kt
+++ app/src/main/java/hu/bme/cryptochecker/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package hu.bme.cryptochecker.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import hu.bme.cryptochecker.R
 import javax.inject.Inject
@@ -13,6 +14,9 @@ class MainActivity : AppCompatActivity() {
     @Inject
     @Named("DummyNetworkService")
     lateinit var networkService: String
+
+    // ViewModel inject
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
