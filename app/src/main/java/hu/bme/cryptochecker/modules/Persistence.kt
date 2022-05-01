90af98d1826b1f64a204e6666d2a0977c47d5875
Bereczki Sándor Máté <bsandor453@gmail.com>
2022-05-01 05:40:01 +0200

    Added modules and persistence

---
+++ app/src/main/java/hu/bme/cryptochecker/modules/Persistence.kt
@@ -0,0 +1,10 @@
+package hu.bme.cryptochecker.modules
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class Persistence {
+}
---
+++ app/src/main/java/hu/bme/cryptochecker/persistence/CryptoDatabase.kt
@@ -0,0 +1,4 @@
+package hu.bme.cryptochecker.persistence
+
+class CryptoDatabase {
+}
